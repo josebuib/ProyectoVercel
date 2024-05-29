223ed550a6baea6847d516758303a6621f566738
josebuib <josebuitragob75@gmail.com>
2024-05-28 21:13:41 -0500

    ajuste configuracion

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to my messages API"})
+    res.json({ "message": "Welcom"})
 })
 
 export default app;
